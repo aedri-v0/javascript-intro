4d0c7c435829074310476d87cb6e931a886c2a81
aedri-v0 <aedri-v0@users.noreply.github.com>
2021-11-23 23:53:51 +0000

    deploy: eb7a8c3cd772ca18ba125248a8c7e05b8786a23f

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 var button = document.querySelector("button");
 
+var submit = document.querySelector("#Submit");
+
+var reset = document.querySelector("#reset");
 // Get reference to unordered list
 var img = document.querySelector("img");
 
@@ -39,14 +42,60 @@ function start() {
   whosThatPokemon();
   //make mc options
   multipleChoice();
+  //creates radio buttons with options
+  makeRadioButtons();
 }
 function whosThatPokemon() {
   randomPoke = pokemon[Math.floor(Math.random() * 4)];
-  document.querySelector("#wtp-sprite").src = randomPoke.sprites.front_default;
+  //document.querySelector("#wtp-sprite").src = randomPoke.sprites.front_default;
+  document.querySelector(
+    "#wtp-sprite"
+  ).src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${randomPoke.id}.png`;
 }
 function multipleChoice() {}
 
+function makeRadioButtons() {
+  let labels = document.querySelectorAll("label");
+
+  for (let i = 0; i < labels.length; i++) {
+    console.log(labels[i]);
+    labels[i].innerText = pokemon[i].name;
+  }
+
+  // pokemon.forEach((pokemon) => {
+  //   let div = document.createElement("div");
+  //   div.innerHTML += `<label>${pokemon.name}</label>`;
+  //   div.innerHTML += `<input type="radio" value="${pokemon.name}" />`
+  //   document.body.appendChild(div);
+  // });
+}
+
+function checkCorrect() {
+  // get value from button that is currently selected
+  let choice = document.querySelector("input[type=radio]:checked");
+  if (!choice) return;
+  choice = parseInt(choice.value);
+  if (pokemon[choice].id === randomPoke.id) {
+    document.getElementById("win").classList.add("active");
+    document.getElementById("lose").classList.remove("active");
+    document.getElementById("reset").classList.add("active");
+    document.getElementById("wtp-sprite").classList.add("solved");
+  } else {
+    document.getElementById("lose").classList.add("active");
+    document.getElementById("win").classList.remove("active");
+    document.getElementById("reset").classList.add("active");
+    document.getElementById("wtp-sprite").classList.add("solved");
+  }
+}
+
+function restart() {
+  location.reload();
+}
+
 // Define the instructions
-button.addEventListener("click", getData);
+
+submit.addEventListener("click", checkCorrect);
+
+reset.addEventListener("click", restart);
 
 getData();
